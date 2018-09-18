efe096b44cf5fd3836fe7fc094b7c3103733d96b
Manisha Tiwari <manisha.ti@HCL.COM>
2018-09-18 10:47:25 +0530

    manisha

--- src/test/java/test/pivotal/pal/tracker/EnvControllerTest.java
+++ src/test/java/test/pivotal/pal/tracker/EnvControllerTest.java
@@ -11,14 +11,13 @@ public class EnvControllerTest {
     @Test
     public void getEnv() throws Exception {
         EnvController controller = new EnvController(
-            "8675",
-            "12G",
-            "34",
-            "123.sesame.street"
+                "8675",
+                "12G",
+                "34",
+                "123.sesame.street"
         );
 
         Map<String, String> env = controller.getEnv();
-
         assertThat(env.get("PORT")).isEqualTo("8675");
         assertThat(env.get("MEMORY_LIMIT")).isEqualTo("12G");
         assertThat(env.get("CF_INSTANCE_INDEX")).isEqualTo("34");
